91b1a6c09347f5aad4cb65a37d58067e6cfcc3a8
FFudo <philipphofbauer@aon.at>
2025-01-05 12:57:01 +0000

    Saved the json data as json file

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import json
+
 from nasa_api import NasaDataManager
+
 
 def run_app():
     nasa_data = NasaDataManager()
+    data = nasa_data.get_apod()
+    with open("output.json", "w") as f:
+        json.dump(data, f, indent=6)
 
 
 if __name__ == "__main__":
